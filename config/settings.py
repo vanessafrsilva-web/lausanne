4b64fd03591c486a5ca4babb210ce49fc19a5689
vanessafrsilva-web <vanessafrsilva@gmail.com>
2026-03-13 08:58:18 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,23 @@
 
 # --- CONFIGURATION ---
 BUREAU_ADRESSE = "Chemin Mont-Paisible 18, 1011 Lausanne"
+
 BUREAU_GPS = (46.5332, 6.6135) 
+
 AGENTS = ["Celine", "Maria Claret", "Maria Elisabeth"]
+
+INFOS_BATIMENTS = {
+    'Bethusy A': {'rue': 'Avenue de Béthusy 54, Lausanne', 'lat': 46.5225, 'lon': 6.6472},
+    'Bethusy B': {'rue': 'Avenue de Béthusy 56, Lausanne', 'lat': 46.5227, 'lon': 6.6475},
+    'Montolieu A': {'rue': 'Isabelle-de-Montolieu 90, Lausanne', 'lat': 46.5412, 'lon': 6.6421},
+    'Montolieu B': {'rue': 'Isabelle-de-Montolieu 92, Lausanne', 'lat': 46.5415, 'lon': 6.6425},
+    'Tunnel': {'rue': 'Rue du Tunnel 17, Lausanne', 'lat': 46.5255, 'lon': 6.6328},
+    'Oron': {'rue': "Route d'Oron 77, 1010 Lausanne", 'lat': 46.5361, 'lon': 6.6625}
+}
+
+SECTEURS = {
+    'Bethusy': ['Bethusy A', 'Bethusy B'],
+    'Montolieu': ['Montolieu A', 'Montolieu B'],
+    'Tunnel': ['Tunnel'],
+    'Oron': ['Oron']
+}
