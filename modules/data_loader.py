bb258a5675dc145a5c0f72edec256cfddcf4b8a2
vanessafrsilva-web <vanessafrsilva@gmail.com>
2026-03-13 09:30:18 +0100

    Update data_loader.py

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,1 +1,18 @@
 
+import pandas as pd
+
+
+def charger_logements(file):
+    """
+    Charge un fichier CSV ou Excel contenant les logements.
+    """
+
+    if file.name.endswith(".csv"):
+        df = pd.read_csv(file)
+    else:
+        df = pd.read_excel(file)
+
+    # nettoyer les colonnes
+    df.columns = df.columns.str.strip()
+
+    return df
