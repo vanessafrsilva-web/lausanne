6bfb3fa72a5f8d774fbc41fba191090972701854
vanessafrsilva-web <vanessafrsilva@gmail.com>
2026-03-13 09:05:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,25 +16,6 @@ def trouver_secteur(batiment):
         if batiment in liste: return secteur
     return batiment 
 
-def generer_ics(df_agent):
-    ics_content = "BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:-//Unite Logement//Planning//FR\n"
-    for _, row in df_agent.iterrows():
-        try:
-            date_obj = datetime.strptime(row['Date'], '%d/%m/%Y')
-            heure_obj = datetime.strptime(row['Heure'], '%H:%M')
-            start = date_obj.replace(hour=heure_obj.hour, minute=heure_obj.minute)
-            end = start + timedelta(minutes=60)
-            ics_content += "BEGIN:VEVENT\n"
-            ics_content += f"SUMMARY:Mission {row['ID']} - {row['Batiment']}\n"
-            ics_content += f"DTSTART:{start.strftime('%Y%m%dT%H%M%S')}\n"
-            ics_content += f"DTEND:{end.strftime('%Y%m%dT%H%M%S')}\n"
-            ics_content += f"LOCATION:{row['Rue']}\n"
-            ics_content += f"DESCRIPTION:Type: {row['Type']}\n"
-            ics_content += "END:VEVENT\n"
-        except: continue
-    ics_content += "END:VCALENDAR"
-    return ics_content
-
 if 'db' not in st.session_state:
     st.session_state.db = pd.DataFrame(columns=['ID', 'Batiment', 'Date', 'Heure', 'Agent', 'Rue', 'Type', 'Statut', 'Date_Sort'])
 
